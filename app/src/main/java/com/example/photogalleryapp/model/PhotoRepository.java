21ad7e6a8fdc4afcdf8449ec80fd9e3531d48eb8
Spice <55505045+ReleaseTheSpice@users.noreply.github.com>
2021-10-24 21:11:05 -0700

    Refactored findPhotos to use lambda functions / streams

--- app/src/main/java/com/example/photogalleryapp/model/PhotoRepository.java
+++ app/src/main/java/com/example/photogalleryapp/model/PhotoRepository.java
@@ -1,11 +1,19 @@
 package com.example.photogalleryapp.model;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Environment;
 
+import androidx.annotation.RequiresApi;
+
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.stream.Stream;
 
 public class PhotoRepository implements IPhotoRepository{
     private Context context;
@@ -29,23 +37,50 @@ public class PhotoRepository implements IPhotoRepository{
         return photos.get(photos.size() - 1).getPhotoPath();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public ArrayList<Photo> findPhotos(Date startTimestamp, Date endTimestamp, String keywords, String locate) {
         photos = new ArrayList<Photo>();
         File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "/Android/data/com.example.photogalleryapp/files/Pictures");
         File[] fList = file.listFiles();
+
         if(fList != null){
-            for (File f : fList) {
-                if (((startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime()
-                        && f.lastModified() <= endTimestamp.getTime()))
-                        && (keywords.equals("") || f.getPath().contains(keywords))
-                        && (locate.equals("") || f.getPath().contains(locate))) {
-                    PhotoCreate(f.getPath());
-                }
-            }
+            Stream<File> fileStream = Arrays.stream(fList);
+            fileStream.filter(f -> {
+                return (startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime());
+            }).filter(f -> {
+                return f.lastModified() <= endTimestamp.getTime();
+            }).filter(f -> {
+                return (keywords.equals("") || f.getPath().contains(keywords));
+            }).filter(f -> {
+                return (locate.equals("") || f.getPath().contains(locate));
+            }).forEach(f -> {
+                PhotoCreate(f.getPath());
+            });
             return photos;
         } else {
             return null;
         }
     }
+
+//    public ArrayList<Photo> findPhotos(Date startTimestamp, Date endTimestamp, String keywords, String locate) {
+//        photos = new ArrayList<Photo>();
+//        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "/Android/data/com.example.photogalleryapp/files/Pictures");
+//        File[] fList = file.listFiles();
+//        Stream<File> fileStream = file.listFiles().stream();
+//
+//        if(fList != null){
+//            for (File f : fList) {
+//                if (((startTimestamp == null && endTimestamp == null) || (f.lastModified() >= startTimestamp.getTime()
+//                        && f.lastModified() <= endTimestamp.getTime()))
+//                        && (keywords.equals("") || f.getPath().contains(keywords))
+//                        && (locate.equals("") || f.getPath().contains(locate))) {
+//                    PhotoCreate(f.getPath());
+//                }
+//            }
+//            return photos;
+//        } else {
+//            return null;
+//        }
+//    }
 
 }
